f3d6390bd948b037b75295112ea51753063929ea
eduardo <eduardofort27@gmail.com>
2025-10-17 22:37:27 -0300

    alteraçoes no projeto 2

--- src/metodos.java
+++ src/metodos.java
@@ -2,13 +2,15 @@ public class metodos {
     public static void main (String [] args) {
         String primeiroNome = "Eduardo";
         String segundoNome = "Vitor";
+        String espaço = " 25 anos";
 
-        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome, espaço);
         System.out.println(nomeCompleto);
     }
 
-    public static String nomeCompleto (String primeiroNome, String segundoNome) {
-        return primeiroNome + (segundoNome);
+    public static String nomeCompleto (String primeiroNome, String segundoNome, String espaço) {
+        return primeiroNome + (segundoNome) + espaço;
+
 
     }
 }
