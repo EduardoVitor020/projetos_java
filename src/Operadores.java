a98a0094e481c26b757888eb84e6164341d8c65c
eduardo <eduardofort27@gmail.com>
2025-10-20 22:06:43 -0300

    aprendendo sobre variaveis e operadores

---
+++ src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    
+    public static void main (String [] args ){
+        
+        double soma = 100 + 50;
+        int subtração = 100 - 50;
+        int multiplicação = 100 * 50;
+        double divisão = 100.0 / 50.0;
+        int modulo = 100 % 50;
+        
+    }
+}
---
+++ src/variaveis.java
@@ -0,0 +1,14 @@
+public class variaveis {
+    
+    public static void main (String [] args){
+         double salarioMinimo = 2.500; //estou dizendo que o número é 2,500, o ponto referece a uma virgula
+        double salarioDiferente = 2500; //aqui eu falo que o número 2500 é dois mil e quinhetos completo, 
+        //pois sem a virgula o número fica inteiro
+        
+
+        final int VALOR_COMUM =  10;
+        
+
+    
+    }
+}
